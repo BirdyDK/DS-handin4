b43adf4b9aa227c2290dbc5c7d960ec0e4256128
Cybermanboss <matal@itu.dk>
2024-11-10 15:49:47 +0100

    hell of a lot of print statements

--- node/node.go
+++ node/node.go
@@ -44,7 +44,9 @@ func NewNode(id, address string, nodes []string) *Node {
 }
 
 func (n *Node) RequestAccess(nodeID string, timestamp int64) bool {
+	log.Println(n.ID + " locking RequestAccess")
 	n.mutex.Lock()
+	log.Println(n.ID + "  RequestAccess locked")
 	defer n.mutex.Unlock()
 
 	// Update the node's logical clock
@@ -52,11 +54,13 @@ func (n *Node) RequestAccess(nodeID string, timestamp int64) bool {
 	if n.State == STATE_HELD || (n.State == STATE_WANTED && (n.timestamp < timestamp || (n.timestamp == timestamp && n.ID < nodeID))) {
 		// Defer the reply
 		n.deferredReplies = append(n.deferredReplies, nodeID)
+		log.Println(n.ID + " unlocking RequestAccess")
 		return false
 	}
 
 	// Update the state to HELD
 	n.State = STATE_HELD
+	log.Println(n.ID + " unlocking RequestAccess")
 	return true
 }
 
@@ -65,23 +69,29 @@ func (n *Node) CanEnterCriticalSection() bool {
 	defer n.mutex.Unlock()
 
 	// Node can enter the Critical Section if it is in the WANTED state
+	log.Println(n.ID + " unlocking CanEnterCriticalSection")
 	return n.State == STATE_WANTED
 }
 
 func (n *Node) ReleaseAccess(nodeID string) {
 	n.mutex.Lock()
 	defer n.mutex.Unlock()
-
+	log.Println(n.ID + " locked releaseaccess")
 	n.State = STATE_REST
+	log.Println(n.ID + " state is rest")
 	for _, node := range n.deferredReplies {
 		n.sendReply(node)
 	}
+	log.Println(n.ID + " sent reqplies")
 	n.deferredReplies = []string{}
 	n.csAccess <- true
+	log.Println(n.ID + " unlocking ReleaseAccess")
 }
 
 func (n *Node) NotifyNodesEntering() {
+	log.Printf("attempting to lock ")
 	n.mutex.Lock()
+	log.Printf("finished locking")
 	defer n.mutex.Unlock()
 
 	n.State = STATE_WANTED
@@ -89,13 +99,17 @@ func (n *Node) NotifyNodesEntering() {
 
 	for _, client := range n.clients {
 		go func(client proto.NodeClient) {
+
 			req := &proto.AccessRequest{NodeId: n.ID, Timestamp: n.timestamp}
+			log.Println(n.ID + " attempting to reqeustaccess")
 			_, err := client.RequestAccess(context.Background(), req)
+			log.Println(n.ID + " requestaccess finished")
 			if err != nil {
 				log.Printf("Error while sending access request: %v", err)
 			}
 		}(client)
 	}
+	log.Println(n.ID + " unlocking NotifyNodesEntering")
 }
 
 func (n *Node) NotifyNodesLeaving() {
@@ -107,23 +121,30 @@ func (n *Node) NotifyNodesLeaving() {
 	for _, client := range n.clients {
 		go func(client proto.NodeClient) {
 			req := &proto.AccessRelease{NodeId: n.ID}
+			log.Println(n.ID + " attempting to releaseaccess")
 			_, err := client.ReleaseAccess(context.Background(), req)
+			log.Println(n.ID + " finished releaseaccess")
 			if err != nil {
 				log.Printf("Error while sending release notification: %v", err)
 			}
 		}(client)
 	}
+	log.Println(n.ID + " unlocking NotifyNodesLeaving")
 }
 
 func (n *Node) sendReply(nodeID string) {
+	log.Println(n.ID + " attempting to connect client to node")
 	client, conn := NewGRPCClient(nodeID)
 	defer conn.Close()
+	log.Println(n.ID + " succeeded in connection")
 
 	req := &proto.AccessRequest{NodeId: n.ID, Timestamp: n.timestamp}
+	log.Println(n.ID + " attempting client.requestAccess")
 	_, err := client.RequestAccess(context.Background(), req)
 	if err != nil {
 		log.Printf("Error while sending reply: %v", err)
 	}
+	log.Println(n.ID + " succeeded in requestAccess")
 }
 
 func max(a, b int64) int64 {
